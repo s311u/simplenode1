b0663e5b1d71789c3e5118b1f52dcbc170aa1606
Selina Kallio <selina.kallio@gmail.com>
2022-10-11 16:02:43 +0300

    pushing finished work from VSC3

---
+++ server/index.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+
+const app = express();
+
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Node server is responding' });
+});
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
